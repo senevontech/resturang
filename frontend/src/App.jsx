fce0029c9aba6912a92c79fa38ebc3263db0f958
Srijon-Karmakar <srijonkarmakar333@gmail.com>
2026-01-05 13:40:33 +0530

    again routing uissue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,73 +3,6 @@
 
 
 
-// import React, { useEffect } from "react";
-// import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
-// import Lenis from "lenis";
-
-// import Home from "./pages/Home";
-// import MenuCategories from "./pages/MenuCategories";
-
-// export default function App() {
-//   useEffect(() => {
-//     const lenis = new Lenis({
-//       smoothWheel: true,
-//       lerp: 0.08,
-//     });
-
-//     function raf(time) {
-//       lenis.raf(time);
-//       requestAnimationFrame(raf);
-//     }
-//     requestAnimationFrame(raf);
-
-//     // 🌍 expose globally for MenuOverlay
-//     window.lenis = lenis;
-
-//     return () => {
-//       window.lenis = undefined;
-//       lenis.destroy();
-//     };
-//   }, []);
-
-//   return (
-//     <HashRouter>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/menus" element={<MenuCategories />} />
-//       </Routes>
-//     </BrowserRouter>
-//     </HashRouter>
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React, { useEffect } from "react";
 import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import Lenis from "lenis";
@@ -100,11 +33,41 @@ export default function App() {
   }, []);
 
   return (
-    <HashRouter>
+
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menus" element={<MenuCategories />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
+  
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
